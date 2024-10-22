14f0348a809e5c57474c34a7dab36c61026130b7
ismeh <gadeismael@hotmail.com>
2024-10-22 07:43:09 +0200

    seguridad

---
+++ src/main/java/com/dss/carrito/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.dss.carrito;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests -> {
+                    authorizeRequests
+                            .requestMatchers("/", "/productos", "/cart/**", "api/products").permitAll()
+                            .requestMatchers("/admin/**", "/form/producto").hasRole("ADMIN")
+                            .anyRequest().authenticated();
+                })
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout")
+                );
+        return http.build();
+    }
+}
